4468f1ad9d2630c0852a554e134ab9758779f1c3
Shuky_eh41 <Shuky_eh41>
2022-08-22 10:28:29 +0400

    gethub

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-a = input("Enter your number")
-if a == a[::-1]:
-    print("Palindrom")
-else:
-    print("Ne polinrom")
+num1 = int(input("Enter your number"))
+num2 = int(input("Enter your number"))
+
+for i in range(num1,num2):
+
+    if i%2==0:
+        print(i)
+
+    if i%2==1:
+        print(i)
+
+    elif i%9==0:
+        print(i)
